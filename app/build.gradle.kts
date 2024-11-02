24779a2c84b3c0284a396a1a47f1b247ed95c7ac
Ruben Renart <rubenrenart@gmail.com>
2024-11-02 01:41:36 +0100

    v0.3 - Botones 4 y 5 funcionales

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.appextra1_menu"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.appextra1_menu"
--- app/src/main/java/com/example/appextra1_menu/MainActivity.kt
+++ app/src/main/java/com/example/appextra1_menu/MainActivity.kt
@@ -1,20 +1,77 @@
 package com.example.appextra1_menu
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.collection.emptyLongSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_1:AppCompatButton
+    lateinit var btn_2:AppCompatButton
+    lateinit var btn_3:AppCompatButton
+    lateinit var btn_4:AppCompatButton
+    lateinit var btn_5:AppCompatButton
+    lateinit var tv_btn_5:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        initComponents()
+        initListeners()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun initComponents()
+    {
+        btn_1 = findViewById(R.id.btn_1)
+        btn_2 = findViewById(R.id.btn_2)
+        btn_3 = findViewById(R.id.btn_3)
+        btn_4 = findViewById(R.id.btn_4)
+        btn_5 = findViewById(R.id.btn_5)
+        tv_btn_5 = findViewById(R.id.tv_btn_5)
+    }
+
+    fun initListeners()
+    {
+        btn_1.setOnClickListener()
+        {
+
+        }
+
+        btn_2.setOnClickListener()
+        {
+
+        }
+
+        btn_3.setOnClickListener()
+        {
+
+        }
+
+        btn_4.setOnClickListener()
+        {
+            finishAffinity()
+        }
+
+        btn_5.setOnClickListener()
+        {
+            if (tv_btn_5.visibility == View.VISIBLE)
+                tv_btn_5.visibility = View.INVISIBLE
+            else
+                tv_btn_5.visibility = View.VISIBLE
         }
     }
 }
